ed91a31d3d51136597ed86f6b0eb14247e11c787
nilsmanc <etereaiberia@gmail.com>
2023-11-23 08:02:39 +0300

    header elements

---
+++ js/appHeader.js
@@ -0,0 +1,38 @@
+export const createHeader = (city) => {
+  const header = document.createElement('header')
+  const headerContainer = document.createElement('div')
+  const headerCity = document.createElement('div')
+  const headerUnits = document.createElement('div')
+  const cityChange = document.createElement('button')
+  const cityLocation = document.createElement('button')
+  const cityName = document.createElement('h1')
+  const unitsC = document.createElement('button')
+  const unitsF = document.createElement('button')
+  const cityInner = document.createElement('div')
+  const searchBlock = document.createElement('div')
+  const searchInput = document.createElement('input')
+  const searchBtn = document.createElement('button')
+  const errorBlock = document.createElement('p')
+
+  header.classList.add('header')
+  headerContainer.classList.add('container', 'header__container')
+  headerCity.classList.add('header__city')
+  headerUnits.classList.add('header__units')
+  cityChange.classList.add('city__change', 'btn-reset')
+  cityLocation.classList.add('city__location', 'btn-reset')
+  cityName.classList.add('city__name')
+  cityInner.classList.add('city__inner')
+  unitsC.classList.add('units__c', 'btn-reset', 'unit-current')
+  unitsF.classList.add('units__f', 'btn-reset')
+  searchBlock.classList.add('search')
+  searchInput.classList.add('search_input')
+  searchBtn.classList.add('search_btn')
+  errorBlock.classList.add('search__error')
+
+  searchBtn.textContent = 'ok'
+  cityName.textContent = city
+  cityChange.textContent = 'Change city'
+  cityLocation.textContent = 'My location'
+  unitsC.textContent = 'C'
+  unitsF.textContent = 'F'
+}
